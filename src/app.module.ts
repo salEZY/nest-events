674186973fe0e80a483f39331c8957ff9ba88573
salEZY <bobicaleks@yahoo.com>
2023-02-25 19:04:23 +0100

    add docker, setup postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './app.controller'
 import { EventsController } from './event.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'nest-events',
+        synchronize: false
+      })
+    })
+  ],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
