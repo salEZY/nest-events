7d957f82a501ad7a3a95b1c1ec6dca0a61648d2c
salEZY <bobicaleks@yahoo.com>
2023-07-26 12:56:15 +0200

    implement jwt tokens

--- src/auth/auth.controlerr.ts
+++ src/auth/auth.controlerr.ts
@@ -1,15 +1,19 @@
 import { Controller, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from '@nestjs/passport'
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
 
+    constructor(private readonly authService: AuthService) { }
+
     @Post('login')
     @UseGuards(AuthGuard('local'))
     async login(@Request() req) {
+        console.log(req.user)
         return {
             userId: req.user.id,
-            token: 'TOKEN'
+            token: this.authService.getTokenForUser(req.user)
         }
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./user.entity";
 import { LocalStrategy } from "./local.strategy";
 import { AuthController } from "./auth.controlerr";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthService } from "./auth.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    providers: [LocalStrategy],
+    imports: [TypeOrmModule.forFeature([User]), JwtModule.registerAsync({
+        useFactory: () => ({
+            secret: process.env.JWT_SECRET,
+            signOptions: {
+                expiresIn: '60m'
+            }
+        })
+    })],
+    providers: [LocalStrategy, AuthService],
     controllers: [AuthController]
 })
 export class AuthModule { }
